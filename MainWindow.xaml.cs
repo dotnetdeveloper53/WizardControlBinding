9834198a1ef6dafe3909837aba79eb9d022c82a6
dotnetdeveloper53 <ben.chivers53@gmail.com>
2015-08-12 11:00:00 +0100

    Files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StackOverflowWizardExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class MainViewModel
+    {
+        public PageViewModel Page1VM
+        {
+            get;
+            set;
+        }
+
+        public PageViewModel Page2VM
+        {
+            get;
+            set;
+        }
+
+        public MainViewModel()
+        {
+            this.Page1VM = new PageViewModel("Page 1");
+            this.Page2VM = new PageViewModel("Page 2");
+        }
+    }
+
+    public class PageViewModel
+    {
+        public string Title { get; set; }
+        public PageViewModel(string title) { this.Title = title; }
+    }
+}
---
+++ WizardControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StackOverflowWizardExample
+{
+    /// <summary>
+    /// Interaction logic for WizardControl.xaml
+    /// </summary>
+    public partial class WizardControl : UserControl
+    {
+        public WizardControl()
+        {
+            InitializeComponent();
+        }
+
+        public WizardPageCollection Pages
+        {
+            get { return (WizardPageCollection)GetValue(PagesProperty); }
+            set { SetValue(PagesProperty, value); }
+        }
+
+        public static readonly DependencyProperty PagesProperty =
+            DependencyProperty.Register("Pages", typeof(WizardPageCollection), typeof(WizardControl), new PropertyMetadata(new WizardPageCollection(), new PropertyChangedCallback(Pages_Changed)));
+
+        static void Pages_Changed(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            WizardPageCollection col =  e.NewValue as WizardPageCollection;
+            WizardControl ctrl = obj as WizardControl;
+            ctrl.contentPage.Content = col.First();
+        }
+    }
+
+    public class WizardPageCollection : ObservableCollection<WizardPageBase> { }
+
+    public class WizardPageBase : ContentControl { }
+}
